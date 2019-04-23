f459453f78429fae0a2e9bfc1a1b8fe89697ea8c
rpardo <rpardo@ahora.es>
2019-04-23 16:24:15 +0200

    Add project with custom dll for VB .Net processes.

---
+++ Sample_Project/Sample_Project_Processes/SampleDataProcesses.vb
@@ -0,0 +1,124 @@
+﻿Imports FLEXYGO.Configuration.Tokens
+Imports FLEXYGO.Data
+Imports FLEXYGO.Exceptions
+Imports FLEXYGO.Objects
+Imports FLEXYGO.Processing
+Imports FLEXYGO.Processing.ProcessManager
+Imports FLEXYGO.Utilities.General
+Imports FLEXYGO.Utilities.General.Util
+
+Public Class SampleDataProcesses
+
+
+    ''' <summary>
+    ''' Insert new object
+    ''' </summary>
+    ''' <param name="Entity">User Entity object with all info</param>
+    ''' <param name="Ret">ProcessHelper for returning results</param>
+    Public Shared Function InsertObj(Entity As EntityObject, Ret As ProcessHelper) As Boolean
+        Try
+
+            If Not Entity.CanInsert Then
+                Ret.LastException = New LocalizedException("You do Not have enough credentials to insert this Object.")
+                Return False
+            End If
+
+            ''Do something here before insert
+
+
+            If Entity.InsertProcess(Entity.TableName, Settings.ObjectSettings.eUpdateType.Standard, "") Then
+
+                ''Do something here after insert
+
+                Ret.Success = True
+                Return True
+            Else
+                Ret.Success = False
+                Ret.LastException = Entity.LastException
+                Return False
+            End If
+
+        Catch ex As Exception
+            Ret.Success = False
+            Ret.LastException = ex
+            Return False
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Update an object.
+    ''' </summary>
+    ''' <param name="Entity">User Entity object with all info</param>
+    ''' <param name="Ret">ProcessHelper for returning results</param>
+    Public Shared Function UpdateObj(Entity As EntityObject, Ret As ProcessHelper) As Boolean
+        Try
+
+            If Not Entity.CanUpdate Then
+                Ret.LastException = New LocalizedException("You do Not have enough credentials to update this Object.")
+                Return False
+            End If
+
+            Dim lTabla As DataTable = Entity.GetData.Tables(0).GetChanges
+
+            'Dim OldValue As String = lTabla.Rows(0)("my_data_field", DataRowVersion.Original)
+            'Dim NewValue As String = lTabla.Rows(0)("my_data_field", DataRowVersion.Current)
+            'Use this sintax to compare old and new values
+
+            ''Do something here begore update.
+
+            If Entity.UpdateProcess(Settings.ObjectSettings.eUpdateType.Standard, "") Then
+
+
+                ''Do something here after update.
+
+                Ret.Success = True
+                Return True
+            Else
+                Ret.Success = False
+                Ret.LastException = Entity.LastException
+                Return False
+            End If
+
+        Catch ex As Exception
+            Ret.Success = False
+            Ret.LastException = ex
+            Return False
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Delete an object
+    ''' </summary>
+    ''' <param name="Entity">User Entity object with all info</param>
+    ''' <param name="Ret">ProcessHelper for returning results</param>
+    Public Shared Function DeleteObj(Entity As EntityObject, Ret As ProcessHelper) As Boolean
+        Try
+
+            If Not Entity.CanDelete Then
+                Ret.LastException = New LocalizedException("You do Not have enough credentials to delete this Object.")
+                Return False
+            End If
+
+            'Do something here before delete
+
+            If Entity.DeleteProcess(Entity.TableName, Settings.ObjectSettings.eUpdateType.Standard, "") Then
+
+                'Do something here after delete
+
+                Ret.Success = True
+                Return True
+            Else
+                Ret.Success = False
+                Ret.LastException = Entity.LastException
+                Return False
+            End If
+
+        Catch ex As Exception
+            Ret.Success = False
+            Ret.LastException = ex
+            Return False
+        End Try
+    End Function
+
+
+End Class
---
+++ Sample_Project/Sample_Project_Processes/SampleUserProcesses.vb
@@ -0,0 +1,62 @@
+﻿Imports FLEXYGO.Configuration.Tokens
+Imports FLEXYGO.Data
+Imports FLEXYGO.Exceptions
+Imports FLEXYGO.Objects
+Imports FLEXYGO.Processing
+Imports FLEXYGO.Processing.ProcessManager
+Imports FLEXYGO.Utilities.General
+Imports FLEXYGO.Utilities.General.Util
+
+Public Class SampleUserProcesses
+
+
+    ''' <summary>
+    ''' Sample object process executed from object 
+    ''' </summary>
+    ''' <param name="Entity">The entity.</param>
+    ''' <param name="Ret">Returns a Process Helper.</param>
+    ''' <returns><c>true</c> if no error, <c>false</c> otherwise.</returns>
+    ''' <exception cref="System.Exception">Returns error</exception>
+    Public Shared Function SampleUserProcess(ByVal Entity As EntityObject, ByRef Ret As ProcessHelper) As Boolean
+        Try
+
+
+            ''Do Something
+
+            Ret.Success = True
+            Ret.SuccessMessage = "Cache reloaded"
+            Return True
+
+        Catch ex As Exception
+            Ret.Success = False
+            Ret.LastException = ex
+            Return False
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' Sample object process executed from object with params
+    ''' </summary>
+    ''' <param name="Entity">The entity.</param>
+    ''' <param name="Ret">Returns a Process Helper.</param>
+    ''' <param name="Param1">Sample string param</param>
+    ''' <param name="Param2">Sample integer param</param>
+    ''' <returns><c>true</c> if no error, <c>false</c> otherwise.</returns>
+    ''' <exception cref="System.Exception">Returns error</exception>
+    Public Shared Function SampleUserProcessWithParams(ByVal Entity As EntityObject, ByRef Ret As ProcessHelper, ByVal Param1 As String, ByVal Param2 As Integer) As Boolean
+        Try
+
+            ''Do Something
+
+            Ret.Success = True
+            Ret.SuccessMessage = "Cache reloaded"
+            Return True
+
+        Catch ex As Exception
+            Ret.Success = False
+            Ret.LastException = ex
+            Return False
+        End Try
+    End Function
+
+End Class
