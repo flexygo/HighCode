fc1813e0502266cfd5bc534dda14aa2d934e7926
Aitor <atorres@ahora.es>
2019-06-19 09:54:35 +0200

    Practice 5

--- Sample_Project/Practices_Processes/Practice5.vb
+++ Sample_Project/Practices_Processes/Practice5.vb
@@ -19,46 +19,6 @@ Namespace Processes
     'Class Practices.
     Partial Public Class Practices
 
-        ''' <summary>
-        ''' Client Insert
-        ''' </summary>
-        ''' <param name="Entity">The entity.</param>
-        ''' <param name="Ret">Returns a Process Helper.</param>
-        ''' <returns><c>true</c> if no error, <c>false</c> otherwise.</returns>
-        ''' <exception cref="System.Exception">Returns error</exception>
-        Public Shared Function DevClientI(ByVal Entity As EntityObject, ByRef Ret As ProcessHelper) As Boolean
-            Try
-                Dim Action As EntityObject
-
-                'Insert client
-                If Entity.InsertProcess(Entity.TableName, Settings.ObjectSettings.eUpdateType.Standard, Nothing) Then
-                    'New action
-                    Action = New EntityObject("Accion", Ret.ConfToken, Entity.DataManager)
-                    'Set values
-                    Action("Date") = Date.Now
-                    Action("EndDate") = Date.Now.AddDays(1)
-                    Action("ActionType") = "EMAIL"
-                    Action("Comment") = "Talk to the customer and get the technical data"
-                    Action("ActionState") = 1
-                    Action("UserName") = Ret.ConfToken.UserSecurity.UserName
-                    Action("IdClient") = Entity.DataManager.GetValue("Select MAX(IdClient) From Client")
-                    Action("IdEmployee") = Entity("IdEmployee")
-                    'Insert action
-                    If Not Action.Insert() Then
-                        Throw New Exception("Error inserting action")
-                    End If
-                Else
-                    Throw New Exception("Error inserting client")
-                End If
-
-                Ret.Success = True
-                Return True
-            Catch ex As Exception
-                Ret.Success = False
-                Ret.LastException = ex
-                Return False
-            End Try
-        End Function
 
         ''' <summary>
         ''' Sample load object/change property custom process.
@@ -69,13 +29,37 @@ Namespace Processes
         ''' <exception cref="System.Exception">Returns error</exception>
         Public Shared Function stateDepencies(ByVal aSender As FormSender, ByRef Ret As ProcessHelper) As List(Of FormAction)
             Try
-                Dim actionResult As New List(Of FormAction) From {
-                    New FormAction With {
-                    .PropertyName = "Myproperty",
-                    .changeValue = True,
-                    .newValue = "1"
-                    }
-                }
+
+                'Define new action after load/change
+                Dim action As New FormAction
+                Dim actionDate As New FormAction
+                action.PropertyName = "BlockReason"
+                actionDate.PropertyName = "BlockDate"
+
+                If aSender.FormValues("IdState") = 2 Then
+                    'Set new values to the properties
+                    action.changeValue = True
+                    action.newValue = "Blocked by admin"
+
+                    actionDate.changeValue = True
+                    actionDate.newValue = Date.Now
+
+                    'Change default class to properties
+                    action.changeClass = True
+                    action.newClass = "box-danger"
+
+                    actionDate.changeClass = True
+                    actionDate.newClass = "box-danger"
+
+
+                End If
+
+
+                ''Append to afterload action array.
+                Dim actionResult As New List(Of FormAction)
+                actionResult.Add(action)
+                actionResult.Add(actionDate)
+
                 Ret.Success = True
                 Return actionResult
             Catch ex As Exception
@@ -84,7 +68,7 @@ Namespace Processes
                 Return Nothing
             End Try
         End Function
-
+    End Class
 End Namespace
 
 
