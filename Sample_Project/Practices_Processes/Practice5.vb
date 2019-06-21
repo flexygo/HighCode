68227dc20e533328b0a1eaa8b73adce7818a311e
Alberto Criado Andres <acriado@ahora.es>
2019-06-21 09:47:32 +0200

    Praactice 5

--- Sample_Project/Practices_Processes/Practice5.vb
+++ Sample_Project/Practices_Processes/Practice5.vb
@@ -19,9 +19,8 @@ Namespace Processes
     'Class Practices.
     Partial Public Class Practices
 
-
         ''' <summary>
-        ''' Sample load object/change property custom process.
+        ''' State Depencies.
         ''' </summary>
         ''' <param name="aSender">FormSender with objectname, processname or reportname, entity if proceeded and current form values.</param>
         ''' <param name="Ret">Returns a Process Helper.</param>
@@ -31,34 +30,31 @@ Namespace Processes
             Try
 
                 'Define new action after load/change
-                Dim action As New FormAction
+                Dim BlockReason As New FormAction
                 Dim actionDate As New FormAction
-                action.PropertyName = "BlockReason"
-                actionDate.PropertyName = "BlockDate"
+                BlockReason.PropertyName = "BlockReason"
+                BlockDate.PropertyName = "BlockDate"
 
                 If aSender.FormValues("IdState") = 2 Then
                     'Set new values to the properties
-                    action.changeValue = True
-                    action.newValue = "Blocked by admin"
+                    BlockReason.changeValue = True
+                    BlockReason.newValue = "Blocked by admin"
 
-                    actionDate.changeValue = True
-                    actionDate.newValue = Date.Now
+                    BlockDate.changeValue = True
+                    BlockDate.newValue = Date.Now
 
                     'Change default class to properties
-                    action.changeClass = True
-                    action.newClass = "box-danger"
+                    BlockReason.changeClass = True
+                    BlockReason.newClass = "box-danger"
 
-                    actionDate.changeClass = True
-                    actionDate.newClass = "box-danger"
-
-
+                    BlockDate.changeClass = True
+                    BlockDate.newClass = "box-danger"
                 End If
-
 
                 ''Append to afterload action array.
                 Dim actionResult As New List(Of FormAction)
-                actionResult.Add(action)
-                actionResult.Add(actionDate)
+                actionResult.Add(BlockReason)
+                actionResult.Add(BlockDate)
 
                 Ret.Success = True
                 Return actionResult
