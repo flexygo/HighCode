83056ccf6ffaeb58ccd2b4d582f3b4f3c53111e1
dlutz <dlutz@ahora.es>
2020-07-22 14:29:00 +0200

    FIX: Reparados scripts de datos de la tabla script.

--- Sample_Project/Sample_Project_DataBD/scripts/data/steps.sql
+++ Sample_Project/Sample_Project_DataBD/scripts/data/steps.sql
@@ -19,7 +19,7 @@ USING (VALUES
  ,(N'Offline 3ª practica',2,N'',3)
  ,(N'Offline 4ª practica',2,N'',4)
  ,(N'Offline 5ª practica',2,N'',5)
- ,(N'Offline 6ª practica',2,N'',5)
+ ,(N'Offline 6ª practica',2,N'',6)
 ) AS Source ([StepId],[CourseId],[Descrip],[Order])
 ON (Target.[StepId] = Source.[StepId] AND Target.[CourseId] = Source.[CourseId])
 WHEN MATCHED AND (
